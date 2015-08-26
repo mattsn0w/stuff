0da1afbf3b759b15c5a328e9ed730e0751d4d452
Matt Snow <mattsnow@gmail.com>
2015-08-26 09:17:38 -0700

    bug fixes
    lots of bug fixes.
    * basic serial port detection.
    * switch from readlines() to read(bytes) on serial fd/buffer to correct
    line garbage.

--- read_RAVEn_Smart_meter.py
+++ read_RAVEn_Smart_meter.py
@@ -13,84 +13,129 @@
 import serial
 import time
 import xml.etree.ElementTree as ET
+import os
+#import pdb
 
 
-epoch30yOffset = 985500000
-
-# Setup serial interface
-serial.port = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
+def epochtodate(seconds):
+	'''Convert an int() epoc value to a tuple, then to human readable timestamp.'''
 
 
-def cmdSerialWrite(cmd, refresh=None):
+def datetoepoch(dateStamp):
+	''' Make a tuple. convert tuple to epoch seconds. Account for 30y offset.'''
+	tupleresult = time.strptime(dateStamp, '%Y-%m-%d %H:%M:%S')
+	results = int(time.mktime(tupleresult)) + 985500000
+	return results
+
+def hextodec(val):
+	'''Convert the hex values returned by the Rainforest automation's device to
+	int() or float() values.'''
+	result = None
+	if val is str():
+		result = int(val, 16)
+	elif val is not str():
+		result = int(val)
+	return result
+
+
+def serialwrite(cmd, refresh=None):
 	'''First command should always be: initialize
-
 	commands: 
-	       get_device_info, get_connection_status, get_current_summation_delivered,
-	       get_schedule, set_schedule, get_meter_info, get_network_info, set_meter_info, get_time,
-	       get_message, confirm_message, get_current_price, set_current_price, get_instantaneous_demand,
-	       get_current_summation_delivered, get_current_period_usage, get_last_period_usage,
-	       set_fast_poll.
+			get_current_price, get_current_summation_delivered, get_current_period_usage.
+			get_schedule, set_schedule.
+	        get_device_info, get_meter_info, set_meter_info.
+	        get_connection_status, get_network_info, get_time,
+	        get_message, confirm_message
+	        get_current_price, set_current_price, get_instantaneous_demand.
+	        get_current_period_usage, get_last_period_usage.
+	        set_fast_poll.
 	'''
+	devices = ['/dev/ttyUSB0', '/dev/ttyUSB1', '/dev/ttyUSB2']
+	for device in devices:
+		if os.path.exists(device):
+			try:
+				ser = serial.Serial(device, 115200, timeout=1)
+				ser.flushInput()
+				ser.flushOutput()
+			except:
+				raise
 	results = '<data>'
 	if refresh:
 		cmd = '''<Command><Name>%s</Name><Refresh>Y</Refresh></Command>''' % (cmd)
 	else:
 		cmd = '''<Command><Name>%s</Name><Refresh>N</Refresh></Command>''' % (cmd)
-	serial.port.write(cmd)
-	for line in serial.port.readlines():
-		line = line.strip('\n')
-		line = line.strip('\r')
-		results += line
+	# Send new command.
+	ser.write(cmd)
+	while True:
+		print 'True and reading...'
+		# While readline and readlines are supported, reading the buffer sequentially ensures
+		# a valid XML element structure.
+		# Remove the \r\ns since we don't need linefeeds.
+		buf = ser.read(1000)
+		buf = buf.replace('\r', '')
+		buf = buf.replace('\n', '')
+		if len(buf) > 0:
+			results += buf
+			#print buf
+			break
 	results += '</data>'
+	if ser.isOpen():
+		ser.close()
 	return results
 
 
-
-# Enumerate Element and get the 'demand' and timestamps
-def parseVals(xmldata):
-	lastTime = 0
-	demand = None
-	for child in root:
-		print '> %s' % child.tag
-		for item in child:
-			# Assign key,val object names for saner reading.
-			key = item.tag
-			if item.text.startswith('0x'):
-				val = int(item.text, 0)
-			else:
-				val = item.text
-			if key == 'Demand':
-				demand = val
-			elif key == 'TimeStamp':
-				now = val
-				diff = now - lastTime
-				lastTime = val
-			elif key == 'DigitsRight':
-				dright = val
-			elif key == 'DigitsLeft':
-				dleft = val
-		if all([demand, now, dright]):
-			demandStr = str(demand)
-			demandStr = '%s.%s' % (demandStr[:-dright], demandStr[-dright:])
-			demand = float(demandStr)
-		print 'Demand: %s \nTime : %s' % (demand, now)
+def xmltodicts(xet):
+	'''Convert string to XML Element Tree to a dictionary with elements as keys.'''
+	results = {}
+	try:
+		root = ET.fromstring(xet)
+		if ET.iselement(root):
+			for e in root:
+				for i in e:
+					print 'key: %s, val: %s' % (i.tag, i.text)
+					if i.text.startswith('0x'):
+						i.text = hextodec(i.text)
+					results[i.tag] = i.text
+	finally:
+		return results
 
 
+# data = cmdSerialWrite('get_current_summation_delivered', True)
 
-data = cmdSerialWrite('get_current_summation_delivered', True)
-
+# Initialize
+serialwrite('initialize')
 # Build big string of XML.
-data = cmdSerialWrite('get_current_price')
-
+data = serialwrite('get_current_price')
 # Create Element object, parsing XML.
-root = ET.fromstring(data)
+
 
 
 for c in root:
 	print '<%s>' % c.tag
 	for i in c:
-		if i.tag == 'TimeStamp':
-			i.text = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(i.text, 0)))
+		if i.tag == 'TimeStamp' or i.tag == 'UTCTime':
+			'''Sometimes this value is a datestamp, sometimes it is a hex value.'''
+			if i.text.startswith('1985'):
+				print 'format is YYYY-MM-DD'
+				now = dateToEpoch(i.text) + epoch30yOffset
+			elif i.text.startswith('0x'):
+				print 'format is hex val'
+				print 'type is %s' % type(i.text)
+				if type(i.text) is str:
+					i.text = int(i.text, 0)
+				now = i.text + epoch30yOffset
+			timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(now)))
+			print 'timestamp create: %s' % timestamp
+		elif i.tag == 'Demand':
+			demand = int(i.text)
+		elif i.tag == 'DigitsRight':
+			print 'i.text in DigitsRight is: %s' % i.text
+			odx = i.text
+			watts = str(int(lastDemand))[:-odx] + '.' + str(int(lastDemand))[-odx:]
+		elif i.tag == 'Price':
+			price = i.text
 		print '\t<%s>\n\t\t%s\n\t</%s>' % (i.tag, i.text, i.tag)
 	print '</%s>' % c.tag
+
+
 
